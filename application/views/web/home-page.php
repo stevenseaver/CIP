c9372091db8487006559cb5f79c58b8996b4fbbc
stevenseaver <56994967+stevenseaver@users.noreply.github.com>
2021-12-08 13:40:24 +0700

    v.0.3.8.21

--- application/views/web/home-page.php
+++ application/views/web/home-page.php
@@ -1,13 +1,13 @@
 	<!-- Begin Page Content -->
 	<div class="no-gutter pt-4 mt-5 bg-light">
 		<!-- Main Card -->
-		<div class="no-gutter text-center">
+		<div class="no-gutter">
 			<div class="card rounded-0 border-0 mb-3">
-				<img class="card-img my-2" src="<?= base_url('asset/') ?>img/undraw_posting_photo.svg" alt="Welcome Image" width="200" height="400">
+				<img class="card-img my-0" src="<?= base_url('asset/') ?>img/profile/profile.jpg" alt="Welcome Image" width="200" height="500">
 				<div class="card-img-overlay d-flex flex-column justify-content-center">
 					<!-- d-flex atur flexible positioning, justify content center/end/top atur posisi vertical object di dalam div ini -->
-					<h1 class="card-title text-primary ml-3">Welcome</h1>
-					<p class="card-text ml-3">Lorem ipsum dolores babi goreng tepung</p>
+					<h1 class="card-title text-primary ml-5 text-left	">Welcome</h1>
+					<p class="card-text ml-5 text-left">Find the best deal on our plastic bag and packaging products here!</p>
 				</div>
 			</div>
 		</div>
@@ -18,10 +18,10 @@
 				<div class="card border-0 mb-4">
 					<div class="card-body">
 						<div class="text-center">
-							<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="<?= base_url('asset/') ?>img/undraw_posting_photo.svg" alt="...">
+							<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 14rem;" src="<?= base_url('asset/') ?>img/undraw_active_options_re_8rj3.svg" alt="...">
 						</div>
 						<h5 class="text-primary">High Quality</h5>
-						<p>Our plastic bag and packaging use high grade material with continually improved formula to achieve highest strength possible. </p>
+						<p>Our plastic bag and packaging use carefully selected recycled material with continually improved formula to achieve highest strength possible. </p>
 						<!-- arahkan ke landing page soal high quality -->
 						<a href="<?= base_url('web/lp1_quality') ?>">Learn More &rarr;</a>
 					</div>
@@ -32,7 +32,7 @@
 				<div class="card border-0 mb-4">
 					<div class="card-body">
 						<div class="text-center">
-							<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="<?= base_url('asset/') ?>img/undraw_posting_photo.svg" alt="...">
+							<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 16rem;" src="<?= base_url('asset/') ?>img/undraw_together_re_a8x4.svg" alt="...">
 						</div>
 						<h5 class="text-primary">Eco-Mindful</h5>
 						<p>We use more than 95% recycled plastic in our materials, creating demand for recycled plastic thus helping to reduce plastic waste in the landfill. </p>
@@ -46,7 +46,7 @@
 				<div class="card border-0 mb-4">
 					<div class="card-body">
 						<div class="text-center">
-							<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="<?= base_url('asset/') ?>img/undraw_posting_photo.svg" alt="...">
+							<img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="<?= base_url('asset/') ?>img/undraw_specs_re_546x.svg" alt="...">
 						</div>
 						<h5 class="text-primary">Highly Customizable</h5>
 						<p>Plastic comes with many shape and sizes. Be unique! Customize your own using our plastic bag and packaging design guidlines. </p>
