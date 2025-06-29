22690043c1510b9c444b7e2c8af28d4e5c626252
stevenseaver <56994967+stevenseaver@users.noreply.github.com>
2025-06-29 20:12:36 +0700

    v2.4.4(c)
    Added:
    - [Core] Asset file

---
+++ asset/js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Recycled Material", "Virgin Material"],
+    datasets: [{
+      data: [85, 15],
+      backgroundColor: ['#4e73df', '#1cc88a'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: true,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: true
+    },
+    cutoutPercentage: 10,
+  },
+});
---
+++ asset/vendor/xendit/docs/Invoice/EwalletType.md
@@ -0,0 +1,36 @@
+# # EwalletType
+Representing the available eWallet channels used for invoice-related transactions.
+
+```php
+use Xendit\Invoice\EwalletType;
+```
+
+
+    ## Enum
+
+    
+        * `OVO` (value: `'OVO'`)
+    
+        * `DANA` (value: `'DANA'`)
+    
+        * `LINKAJA` (value: `'LINKAJA'`)
+    
+        * `PAYMAYA` (value: `'PAYMAYA'`)
+    
+        * `SHOPEEPAY` (value: `'SHOPEEPAY'`)
+    
+        * `GCASH` (value: `'GCASH'`)
+    
+        * `GRABPAY` (value: `'GRABPAY'`)
+    
+        * `ASTRAPAY` (value: `'ASTRAPAY'`)
+    
+        * `NEXCASH` (value: `'NEXCASH'`)
+    
+        * `JENIUSPAY` (value: `'JENIUSPAY'`)
+    
+        * `XENDIT_ENUM_DEFAULT_FALLBACK` (value: `UNKNOWN_ENUM_VALUE`)
+
+If you encounter `UNKNOWN_ENUM_VALUE`, it means that this ENUM is unavailable in your current SDK version. Please upgrade to get the newest ENUM.
+
+[[Back to README]](../../README.md)
